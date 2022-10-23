dd7e40f27ec4d9a899fddf502f558ddb56166db4
Alexandre Tranchant <alexandre.tranchant@gmail.com>
2022-10-23 16:24:20 +0200

    Fixing repositories

--- tests/App/src/Controller/Admin/DashboardController.php
+++ tests/App/src/Controller/Admin/DashboardController.php
@@ -13,7 +13,9 @@ class DashboardController extends AbstractDashboardController
     #[Route('/admin', name: 'admin')]
     public function index(): Response
     {
-        return parent::index();
+
+        return $this->render('admin.html.twig', [
+        ]);
 
         // Option 1. You can make your dashboard redirect to some common page of your backend
         //
--- tests/App/src/Kernel.php
+++ tests/App/src/Kernel.php
@@ -8,4 +8,14 @@ use Symfony\Component\HttpKernel\Kernel as BaseKernel;
 class Kernel extends BaseKernel
 {
     use MicroKernelTrait;
+
+    public function getCacheDir(): string
+    {
+        return dirname(__DIR__).'/../../var/'.$this->environment.'/cache';
+    }
+
+    public function getLogDir(): string
+    {
+        return dirname(__DIR__).'/../../log/'.$this->environment.'/cache';
+    }
 }

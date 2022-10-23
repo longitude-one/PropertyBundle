5945585989aae5478a814031d290e74c871ebb93
Alexandre Tranchant <alexandre.tranchant@gmail.com>
2022-10-23 11:07:08 +0200

    A new symfony application is embedded to test PropertyBundle in EasyAdminBundle

--- tests/App/public/index.php
+++ tests/App/public/index.php
@@ -1,22 +1,9 @@
 <?php
 
-/**
- * This file is part of the LongitudeOne/PropertyBundle
- *
- * PHP 8.1 | Symfony 6.1+
- *
- * Copyright LongitudeOne - Alexandre Tranchant
- * Copyright 2021 - 2022
- */
+use App\Kernel;
 
-namespace LongitudeOne\PropertyBundle\Tests\App;
+require_once dirname(__DIR__).'/vendor/autoload_runtime.php';
 
-require_once __DIR__.'/../../../vendor/autoload.php';
-
-use Symfony\Component\HttpFoundation\Request;
-
-$kernel = new AppKernel('dev', true);
-$request = Request::createFromGlobals();
-$response = $kernel->handle($request);
-$response->send();
-$kernel->terminate($request, $response);
+return function (array $context) {
+    return new Kernel($context['APP_ENV'], (bool) $context['APP_DEBUG']);
+};

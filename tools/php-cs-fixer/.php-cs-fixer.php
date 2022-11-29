f7337e4f2f5bb005264d4702aee88bec4adf7ef2
Alexandre Tranchant <alexandre.tranchant@gmail.com>
2022-11-29 11:57:57 +0100

    Directory structure respected

--- tools/php-cs-fixer/.php-cs-fixer.php
+++ tools/php-cs-fixer/.php-cs-fixer.php
@@ -5,7 +5,8 @@ $header = str_replace("%year%", date('Y'), $header);
 
 $finder = PhpCsFixer\Finder::create()
     ->in([
-        __DIR__ . '/../../PropertyBundle/',
+        __DIR__ . '/../../src/',
+        __DIR__ . '/../../tests/',
     ])
 ;
 
